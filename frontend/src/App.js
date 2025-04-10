987bd0660e65f2f75acb476a2ec59304a071da67
kushagra <155358802+kushfs@users.noreply.github.com>
2025-04-10 13:42:48 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,7 +35,7 @@ function App() {
     setTrend('');
 
     try {
-      const response = await fetch('http://localhost:8000/predict', {
+      const response = await fetch('https://stockwayy.onrender.com/predict', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
